0a0823a49903c0b46bb740b76e61f055906dc796
Alok <alokkyyadav@gmail.com>
2023-06-17 21:05:58 +0530

    "completed2"

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -3,9 +3,6 @@ package com.driver;
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
-    public String getTradeLicenseId() {
-        return tradeLicenseId;
-    }
 
     public void setTradeLicenseId(String tradeLicenseId) {
         this.tradeLicenseId = tradeLicenseId;
